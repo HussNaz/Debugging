aa1044afe28d676688b60e745c9de5fa731ab2cb
HussNaz <nazmulhassanc09@gmail.com>
2024-11-06 13:19:25 +0600

    Debug Practicing

--- src/DebugPractice.java
+++ src/DebugPractice.java
@@ -1,8 +1,10 @@
 
 public class DebugPractice {
     public int calculate(int a, int b) {
-        int sum = a + b;
-        int difference = a - b;
-        return sum * difference;
+        int result = 0;
+        for(int i = 0; i < 5; i++) {
+            result+= a * b;
+        }
+        return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         DebugPractice debugPractice = new DebugPractice();
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter second number: ");
+        int b = scanner.nextInt();
+
         int result = debugPractice.calculate(5, 3);
+
+
         System.out.println("Result: " + result);
     }
 }
